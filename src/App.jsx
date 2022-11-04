9eb532724a7575965bc1290bf2fd7b1002f24b3e
MHC2022 <115625841+MHC2022@users.noreply.github.com>
2022-11-04 19:19:53 +1300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
 export default function App() {
 
-    return (
-      <div className="page">
-        <div className="container">
-          <img src="https://cdn.discordapp.com/attachments/1036057335317135400/1036549387071012874/coin_gif.gif" className="main-gif"></img>
-          <h1 className="welcome-title">Welcome to the home of $MHC</h1>
-            <p className="welcome-text">Meet MHC, The #1 charity-orientated crypto currency that helps fight mental health problems and supports its victims worldwide.</p>
-        
-          <h1 className="mhc-title--2">Why $MHC</h1>
-            <p className="why-text">Mental Health Coin is a way for us to use modern technology in hopes to support people who battle with mental illnesses, especially in a time where mental health is poorly funded. Additional to our wallet set aside for monthly donations we also hope to host MHC events located around the world to bring more awareness to Mental health. Our inspiration has grown from Mental health awareness week <br></br>(26 SEP – 3 OCT) as well as mental health day (10 OCT).</p>
-            <img src="https://media.discordapp.net/attachments/1036057335317135400/1036549136184528926/ribbin.png" className="ribbon-image"></img>
-            <p className="why-text--2">Mental health coin was founded due to the frugal funding in regards to the mental health service in the USA and UK. To pair with this total mental health diagnoses annually have been rising year by year. Harry and Ralph have spotted this rarely spoken-about issue and have decided to implement it into the blockchain with a tough goal to fix the underinvestment made in these countries, we have developed a system so a total of 5% total supply of $MHC (50M tokens) will be put aside and disregards of the current market situation will make monthly donations towards a community chose charity/organization.</p>
+  return (
+    <div className="page">
+      <div className="container">
+        <img src="https://cdn.discordapp.com/attachments/1036057335317135400/1036549387071012874/coin_gif.gif" className="main-gif"></img>
+        <h1 className="welcome-title">Welcome to the home of $MHC</h1>
+          <p className="welcome-text">Meet MHC, The #1 charity-orientated crypto currency that helps fight mental health problems and supports its victims worldwide.</p>
+      
+        <h1 className="mhc-title--2">Why $MHC</h1>
+          <p className="why-text">Mental Health Coin is a way for us to use modern technology in hopes to support people who battle with mental illnesses, especially in a time where mental health is poorly funded. Additional to our wallet set aside for monthly donations we also hope to host MHC events located around the world to bring more awareness to Mental health. Our inspiration has grown from Mental health awareness week <br></br>(26 SEP – 3 OCT) as well as mental health day (10 OCT).</p>
+          <img src="https://media.discordapp.net/attachments/1036057335317135400/1036549136184528926/ribbin.png" className="ribbon-image"></img>
+          <p className="why-text--2">Mental health coin was founded due to the frugal funding in regards to the mental health service in the USA and UK. To pair with this total mental health diagnoses annually have been rising year by year. Harry and Ralph have spotted this rarely spoken-about issue and have decided to implement it into the blockchain with a tough goal to fix the underinvestment made in these countries, we have developed a system so a total of 5% total supply of $MHC (50M tokens) will be put aside and disregards of the current market situation will make monthly donations towards a community chose charity/organization.</p>
 
-          <h1 className="mhc-title--3">Tokenomics</h1>
-            <p>Only 500,000,000 tokens will ever be created, the allocation of the tokens goes as follows:</p>
-            <p>65% (325,000,000) of the total supply will be up for purchase at lunch date.</p>
-            <p>5% (25,000,000) to be allocated for pre-sale purchasers, if coins remain they will be brought to crowd sale. These accounts include unlocked and locked accounts. The accounts with larger quantities of total supply are locked to make the coin more stable as we start out.</p>
-            <p>65% (325,000,000) to be allocated for pre-purchasers, these will be available on our website mentalhealthcoin.xyz when the public sale is released, ideally they will also be available for purchase on other exchanges shortly after.</p>
-            <p>20% (100,000,000) will be allocated towards the MHC team. This is so we can keep funding those who work behind the scenes to keep the coin running for years to come.</p>
-            <p>5% (25,000,000) will be the company charity wallet, where 0.1% of the wallet will be sold into a major altcoin each month and donated to a community-chosen mental health charity.</p>
-            <p>5% (25,000,000) will be allocated towards the coin's growth. This includes an updated website and the MHC holders event in LA.</p>
-            <img src="https://media.discordapp.net/attachments/1036057335317135400/1036543067781607454/toke_pie.png" className="pie-chart"></img>
-          
-          <h1 className="meet-the--team">Meet our founders</h1>
-           <h2>Founder - Harry</h2>
-            <p>Hey I'm Harry, The founder of MHC. Over the course of 5 years I've been studying computational thinking and have dipped in the web3 space here and there. After hearing multiple stories about how poorly mental health is treated in the majority of countries I decided to try and solve the underinvestment that seems to be a general issue. 2020 was when I truly started getting into the web3 / crypto space online and came up with the idea of MHC, a charitable crypto currency.</p>
-           <h2>Lead Developer - Ralph</h2>
-           <h1 className="title-donate">Where we donate</h1>
-            <p className="developer-tab">My name is Ralph and I am the lead developer for MHC. I have a brilliant team of staff working below me to bring you the best service we can. I have worked on a few crypto currencies and NFT’s before that had great success. As I was working on past projects I realized that none of these coins will ever go on to help people. That is when Harry and I came up with the idea of MHC.</p>
-            <p>MHC commits to monthly donations that will take place on the 25th of every month beginning November 2022, our members will be able to vote for the chosen charity each month with a poll that will be posted on any of our social medias. We have placed aside a generous but suitable amount of our total supply of coins that a portion will be taken from and donated to the chosen charity, our donations will remain at 0.1% of $MHC so we do not disrupt our holders. Fans will be able to tune in to see the donations happen live on our <a href="https://www.twitch.tv/mentalhealthcoin">twitch channel.</a></p>
-            <img src="https://cdn.discordapp.com/attachments/1036057335317135400/1036549135878324274/heart_mhc.png" className="heart-image"></img>
-        </div>
+        <h1 className="mhc-title--3">Tokenomics</h1>
+          <p>Only 500,000,000 tokens will ever be created, the allocation of the tokens goes as follows:</p>
+          <p>65% (325,000,000) of the total supply will be up for purchase at lunch date.</p>
+          <p>5% (25,000,000) to be allocated for pre-sale purchasers, if coins remain they will be transfered to the business account. These accounts include unlocked and locked accounts. The accounts with larger quantities of total supply are locked to make the coin more stable as we start out.</p>
+          <p>65% (325,000,000) to be allocated to crowed sale, these will be available on our website mentalhealthcoin.xyz when the public sale is released, in the near future MHC will be available to purchase and trade on larger exchanges</p>
+          <p>20% (100,000,000) will be allocated towards the MHC team. This is so we can keep funding those who work behind the scenes to keep the coin running for years to come. This wallet will also be used to provide capital to the MHC store, events and MHC counselling </p>
+          <p>5% (25,000,000) will be the company charity wallet, where 0.1% of the wallet will be sold into eth or bitcoin and donated to a community-chosen mental health charity.</p>
+          <p>5% (25,000,000) will be allocated towards the coin's growth. This includes an updated website, the MHC holders event in LA and more.</p>
+          <img src="https://media.discordapp.net/attachments/1036057335317135400/1036543067781607454/toke_pie.png" className="pie-chart"></img>
+        
+        <h1 className="meet-the--team">Meet our founders</h1>
+         <h2>Founder - Harry</h2>
+          <p>Hey I'm Harry, The founder of MHC. Over the course of 5 years I've been studying computational thinking and have dipped in the web3 space here and there. After hearing multiple stories about how poorly mental health is treated in the majority of countries I decided to try and solve the underinvestment that seems to be a general issue. 2020 was when I truly started getting into the web3 / crypto space online and came up with the idea of MHC, a charitable crypto currency.</p>
+         <h2>Lead Developer - Ralph</h2>
+         <h1 className="title-donate">Where we donate</h1>
+          <p className="developer-tab">My name is Ralph and I am the lead developer for MHC. I have a brilliant team of staff working below me to bring you the best service we can. I have worked on a few crypto currencies and NFT’s before that had great success. As I was working on past projects I realized that none of these coins will ever go on to help people. That is when Harry and I came up with the idea of MHC.</p>
+          <p>MHC commits to monthly donations that will take place on the 25th of every month beginning November 2022, our members will be able to vote for the chosen charity each month with a poll that will be posted on any of our social medias. We have placed aside a generous but suitable amount of our total supply of coins that a portion will be taken from and donated to the chosen charity, our donations will remain at 0.1% of $MHC so we do not disrupt our holders. Fans will be able to tune in to see the donations happen live on our <a href="https://www.twitch.tv/mentalhealthcoin">twitch channel.</a></p>
+          <img src="https://cdn.discordapp.com/attachments/1036057335317135400/1036549135878324274/heart_mhc.png" className="heart-image"></img>
       </div>
-    );
-  }
+    </div>
+  );
+}
